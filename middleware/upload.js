dd6996f38c691a6bef40fa88138956857ac84b51
mudassardevr <asuspulsar3@gmail.com>
2026-03-09 17:44:05 +0530

    run

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,29 +1,47 @@
-const multer = require('multer');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-//STORAGE config
-const storage = multer.diskStorage({
-    destination : function(req , file , cb) {
-        cb(null , "uploads/")
-    },
-    filename : function(req , file , cb) {
-        cb(null , Date.now() + "-" + file.originalname);
-    },
+//CLOUDINARY STORAGE
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "gallery-app",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
 });
 
-
-// file filter (image only )
-const fileFilter = (req , file , cb) => {
-    if(file.mimetype.startsWith("image")){
-        cb(null , true);
-    } else{
-        cb(new Error("Only Images") , false);
-    }
-
-};
-
+//MULTER UPLOAD
 const upload = multer({
-    storage, 
-    fileFilter
+  storage,
 });
+
 
 module.exports = upload;
+
+
+
+// //STORAGE config
+// const storage = multer.diskStorage({
+//     destination : function(req , file , cb) {
+//         cb(null , "uploads/")
+//     },
+//     filename : function(req , file , cb) {
+//         cb(null , Date.now() + "-" + file.originalname);
+//     },
+// });
+
+// // file filter (image only )
+// const fileFilter = (req , file , cb) => {
+//     if(file.mimetype.startsWith("image")){
+//         cb(null , true);
+//     } else{
+//         cb(new Error("Only Images") , false);
+//     }
+
+// };
+
+// const upload = multer({
+//     storage,
+//     fileFilter
+// });
--- routes/images.js
+++ routes/images.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const upload = require("../middleware/upload");
 const fetchuser = require("../middleware/fetchUser");
 const Image = require("../models/Image");
+const cloudinary = require("../config/cloudinary");
 
 //ROUTER 1 : upload image Route
 router.post(
@@ -11,14 +12,14 @@ router.post(
   upload.single("image"), // field name MUST match frontend
   async (req, res) => {
     try {
-        console.log(req.file);  
+      console.log(req.file);
       if (!req.file) {
         return res.status(400).json({ error: " No file uploaded" });
       }
 
       const image = await Image.create({
         user: req.user.id,
-        imageUrl: req.file.filename,
+        imageUrl: req.file.path,
       });
 
       res.json({ success: true, image });
@@ -29,44 +30,46 @@ router.post(
   },
 );
 
-// ROUTER 2 : Fetch My Images 
-router.get("/my-images" , fetchuser , async(req , res) => {
+// ROUTER 2 : Fetch My Images
+router.get("/my-images", fetchuser, async (req, res) => {
+  try {
+    const image = await Image.find({ user: req.user.id });
 
-    try {
-        const image = await Image.find({ user : req.user.id})
-
-        res.json(image)
-        
-    } catch (error) {
-        console.error(error.message)
-        res.status(500).send("server Error");
-        
-    }
+    res.json(image);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server Error");
+  }
 });
 
 // ROUTER 3 : Delete images
-router.delete("/:id" , fetchuser , async(req , res) => {
-
-    try {
-
+router.delete("/:id", fetchuser, async (req, res) => {
+  try {
     const image = await Image.findOne({
-        _id : req.params.id,
-        user : req.user.id
+      _id: req.params.id,
+      user: req.user.id,
     });
 
-    if(!image){
-        return res.status(404).json({error : "image not found"});
+    if (!image) {
+      return res.status(404).json({ error: "image not found" });
     }
 
+    // extract public_id from URL
+    const urlParts = image.imageUrl.split("/");
+    const fileName = urlParts[urlParts.length - 1];
+    const publicId = "gallery-app" + fileName.split(".")[0];
+
+    //delete from cloudinary
+    await cloudinary.uploader.destroy(publicId);
+
+    // delete from MongoDB
     await image.deleteOne();
 
-    res.json({success : true});
-
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("server Error");
-    }
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server Error");
+  }
 });
 
-
-module.exports = router
+module.exports = router;
