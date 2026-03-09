7bb398100d8b9c557af1499a532805fb7a4f1e80
mudassardevr <asuspulsar3@gmail.com>
2026-03-09 16:24:56 +0530

    root

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ connectToMongo();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Gallery Backend API Running 🚀");
+});
+
 //Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/images", require("./routes/images"));
