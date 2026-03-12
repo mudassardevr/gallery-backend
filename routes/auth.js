795d0ca52f0a09570fdeef1e7c4a88ce9d199242
mudassardevr <asuspulsar3@gmail.com>
2026-03-12 19:32:13 +0530

    cors

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,10 @@ const router = express.Router();
 
 //otp:
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  //service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
